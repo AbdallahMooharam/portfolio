5a036c869f88cab5a6d46961f6ed202e3e6b19fc
Khaled <101639047+khaled899@users.noreply.github.com>
2024-07-17 14:57:48 +0300

    Updates

---
+++ precache-manifest.1766bb05a17defa89d34bc7e91c2278c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ce34b4cdb4900f95b91abd5b9a6174",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b93497f39be36543d6ab",
+    "url": "/portfolio/static/css/main.53c14c05.chunk.css"
+  },
+  {
+    "revision": "715dde40108ed503d939",
+    "url": "/portfolio/static/js/2.29f37ea3.chunk.js"
+  },
+  {
+    "revision": "b93497f39be36543d6ab",
+    "url": "/portfolio/static/js/main.c99a4800.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "da480456820482b9d6942360c723a92e",
+    "url": "/portfolio/static/media/CV Abdo Moharam.da480456.pdf"
+  },
+  {
+    "revision": "22c839a40b581bcf572b773dddf9a16e",
+    "url": "/portfolio/static/media/Education.22c839a4.png"
+  },
+  {
+    "revision": "b5892044e79c273bea7f9fe8e0a75884",
+    "url": "/portfolio/static/media/Games.b5892044.png"
+  },
+  {
+    "revision": "c99e8f4036370e3f8e1f63516e395777",
+    "url": "/portfolio/static/media/Screenshot_1.c99e8f40.png"
+  },
+  {
+    "revision": "44ee6fed56d3c4c59d0d00da77c6aea3",
+    "url": "/portfolio/static/media/Screenshot_2.44ee6fed.png"
+  },
+  {
+    "revision": "f5f6bcc48b3ddf37c2263f1420ae5e9a",
+    "url": "/portfolio/static/media/Screenshot_3.f5f6bcc4.png"
+  },
+  {
+    "revision": "d25fb07cbf2c4b244457a8aa5a2eaa48",
+    "url": "/portfolio/static/media/Screenshot_4.d25fb07c.png"
+  },
+  {
+    "revision": "2ee0d13dbee7d1c756c39527bc5daa87",
+    "url": "/portfolio/static/media/Screenshot_5.2ee0d13d.png"
+  },
+  {
+    "revision": "5491644d60751c046691c3aa511d9fb7",
+    "url": "/portfolio/static/media/Screenshot_6.5491644d.png"
+  },
+  {
+    "revision": "bc49fb7f45af41b789a7740a046f399c",
+    "url": "/portfolio/static/media/Task.bc49fb7f.jpg"
+  },
+  {
+    "revision": "322b0b1a02f085fa4b1168a0abfd69c5",
+    "url": "/portfolio/static/media/Wooster.322b0b1a.png"
+  },
+  {
+    "revision": "7e6550b5f244290939ab32454e3dfb16",
+    "url": "/portfolio/static/media/portfolio3.7e6550b5.jpg"
+  }
+]);
